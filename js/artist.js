e9df0e5d93ef5f4e21bde982f588ac30fef500fc
illiaGoncharov <ilia.m.goncharov@gmail.com>
2021-03-01 19:29:03 +0300

    fix: теперь клик на артиста откр/закр дроп && новый дроп закрывает старые && плавный скрол к началу чтения

--- js/artist.js
+++ js/artist.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
 	const pD = function(e) {
 		e.preventDefault();
 	};
+    const hideOthers = function(item) {
+        $('.artist__dropdown').not(item).addClass('artist__dropdown-hidden');
+    }
     $('.artist > a').click(function (e) {
     	pD(e);
-    	$('.artist__dropdown').addClass('artist__dropdown-hidden');
-    	$(this).siblings('.artist__dropdown').removeClass('artist__dropdown-hidden'); 
+        $(this).siblings('.artist__dropdown').toggleClass('artist__dropdown-hidden');
+        hideOthers($(this).siblings('.artist__dropdown'));
+        e.target.scrollIntoView({ 
+          behavior: 'smooth' 
+        });
     });
     $('.partner a').click(function (e) {
     	e.preventDefault();
