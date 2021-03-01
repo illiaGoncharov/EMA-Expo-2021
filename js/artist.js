b06012c27635677d1d40ccbc822825b388593102
illiaGoncharov <ilia.m.goncharov@gmail.com>
2021-03-01 14:51:49 +0300

    fix: адрес скрипта [en to root] +++ && feat: в title объявление языков добавил

--- js/artist.js
+++ js/artist.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 	};
     $('.artist > a').click(function (e) {
     	pD(e);
-    	$(!this && '.artist__dropdown').addClass('artist__dropdown-hidden');
+    	$('.artist__dropdown').addClass('artist__dropdown-hidden');
     	$(this).siblings('.artist__dropdown').removeClass('artist__dropdown-hidden'); 
     });
     $('.partner a').click(function (e) {
